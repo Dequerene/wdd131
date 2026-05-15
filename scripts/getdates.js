f5398149297fcd1cf478c4c552690ef1108f4f5f
Osvaldo João <osvaldodequerene@gmail.com>
2026-05-14 21:08:13 -0600

    fixed year and last modified in footer

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,5 +1,7 @@
-const currentYear = document.querySelector("#currentyear");
-const lastModified = document.querySelector("#lastModified");
+document.addEventListener("DOMContentLoaded", () => {
+  const currentYear = document.querySelector("#currentyear");
+  const lastModified = document.querySelector("#lastModified");
 
-currentYear.textContent = new Date().getFullYear();
-lastModified.textContent = `Last Modification: ${document.lastModified}`;
+  currentYear.textContent = new Date().getFullYear();
+  lastModified.textContent = `Last Modification: ${document.lastModified}`;
+});
